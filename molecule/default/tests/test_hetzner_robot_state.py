44417d6c3a1928db5c48e0a0ccce12e4aedf63ac
build-failure <michael@lewkowski.de>
2019-11-19 16:36:29 +0100

    Finalize default test

---
+++ molecule/default/tests/test_hetzner_robot_state.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import unittest
+
+from requests.auth import HTTPBasicAuth
+
+
+class DefaultTest(unittest.TestCase):
+
+    hetzner_robot_base_url = os.getenv(
+        'HETZNER_ROBOT_BASE_URL', 'http://localhost:3000'
+    )
+    auth = HTTPBasicAuth('robot', 'secret')
+
+    def test_firewall_templates_amount_unchanged(self):
+        response = requests.get(self.hetzner_robot_base_url +
+                                "/firewall/template", auth=self.auth)
+        self.assertEqual(len(response.json()), 0)
+
+    def test_firewall_amount_unchanged(self):
+        response = requests.get(self.hetzner_robot_base_url +
+                                "/firewall", auth=self.auth)
+        self.assertEqual(len(response.json()), 0)
