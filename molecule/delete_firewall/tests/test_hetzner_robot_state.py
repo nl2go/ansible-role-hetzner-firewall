462cd17b51e22cf6e7477eefc98259accfd41239
build-failure <michael@lewkowski.de>
2019-11-20 09:57:43 +0100

    Add delete firewall scenario

---
+++ molecule/delete_firewall/tests/test_hetzner_robot_state.py
@@ -0,0 +1,47 @@
+import os
+import requests
+import unittest
+
+from requests.auth import HTTPBasicAuth
+
+
+class DefaultTest(unittest.TestCase):
+    hetzner_robot_base_url = os.getenv(
+        'HETZNER_ROBOT_BASE_URL', 'http://localhost:3000'
+    )
+    auth = HTTPBasicAuth('robot', 'secret')
+
+    def test_firewall_templates_unchanged(self):
+        response = requests.get(self.hetzner_robot_base_url +
+                                "/firewall/template", auth=self.auth)
+
+        self.assertEqual(len(response.json()), 1)
+        self.assertDictEqual(response.json()[0], {
+            'firewall_template': {
+                'id': 1,
+                'name': 'Existing Template',
+                'whitelist_hos': True,
+                'is_default': False,
+                'rules': {
+                    'input': [{
+                        'action': 'accept',
+                        'ip_version': 'ipv4',
+                        'name': 'Allow all'}]
+                }}})
+
+    def test_firewall_amount_unchanged(self):
+        response = requests.get(self.hetzner_robot_base_url +
+                                "/firewall", auth=self.auth)
+        self.assertEqual(len(response.json()), 1)
+        self.assertDictEqual(response.json()[0], {
+            'firewall': {
+                'server_ip': '123.123.123.123',
+                'status': 'active',
+                'whitelist_hos': True,
+                'rules': {
+                    'input': [{
+                        'action': 'accept',
+                        'ip_version': 'ipv4',
+                        'name': 'Allow all'}]
+                }
+            }})
